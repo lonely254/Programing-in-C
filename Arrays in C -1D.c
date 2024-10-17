f3f9f8c8a827b31136d45ac2491642f7864b7097
lonely254 <lonelysaam@gmail.com>
2024-10-17 11:05:29 +0300

    push the project

---
+++ Arrays in C -1D.c
@@ -0,0 +1,11 @@
+//Arrays in C -1D
+#include <stdio.h>
+int main(int argc, char** argv)
+{
+	int i;
+	int marks[5]={50,90,60,70,40};
+	for(i=0;i<5;i++){
+		printf("marks[%d]=%d\n",i,marks[i]);
+	}
+	return 0;
+}
